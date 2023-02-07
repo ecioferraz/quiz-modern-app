7ff89fdb56f20581f85dfbe5773b91dda68f6ce9
Écio Ferraz <ecioferraz@hotmail.com>
2023-02-07 16:18:54 -0300

    feat: router

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { theme } from './styles/theme';
 import '../fonts/index.css';
+import Router from './router/Router';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <p>Teste</p>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import Detail from '../pages/Detail';
+import Home from '../pages/Home';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path='' element />
-      <Route path='/detail/:category' element />
+      <Route path='' element={<Home />} />
+      <Route path='/detail' element={<Detail />} />
       <Route path='/quiz' element />
     </Routes>
   );
