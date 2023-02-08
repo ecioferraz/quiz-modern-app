dfe92e64e0f2bb4a62374f64a81a297fde23e975
Écio Ferraz <ecioferraz@hotmail.com>
2023-02-08 11:57:22 -0300

    refactor: Router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Detail from '../pages/Detail';
 import Home from '../pages/Home';
+import Quiz from '../pages/Quiz';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path='' element={<Home />} />
-      <Route path='/detail' element={<Detail />} />
-      <Route path='/quiz' element />
+      <Route path="" element={<Home />} />
+      <Route path="/detail" element={<Detail />} />
+      <Route path="/quiz" element={<Quiz />} />
     </Routes>
   );
 }
